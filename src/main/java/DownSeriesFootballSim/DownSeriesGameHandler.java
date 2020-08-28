f8bc97cffd7ae3306167f7272e295d956b6b2c2a
Rick Wight <obpwilliams482@gmail.com>
2020-08-28 16:42:43 -0400

    now a maven project

---
+++ src/main/java/DownSeriesFootballSim/DownSeriesGameHandler.java
@@ -0,0 +1,10 @@
+package DownSeriesFootballSim;
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.LambdaLogger
+
+import java.util.Map;
+
+public class DownSeriesGameHandler implements RequestHandler<Map<String, String>, String> {
+}
